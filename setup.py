621f1ec5ace269557a3364a1dc1c2027d8d3eb46
Dario Steiner <steiner.stdio@gmail.com>
2017-08-02 15:15:15 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -17,12 +17,10 @@ options = {
 }
 
 setup(
-    name='PyXlProjectTemplate',
-    version='0.1dev',
-    packages=['project_template', ],
-    license='Dario Steiner license',
-    long_description=open('README.md').read(),
+    name='PythonProjectTemplate',
     options=options,
+    version='0.1dev',
     description='<any description>',
+    long_description=open('README.md').read(),
     executables=executables
 )
