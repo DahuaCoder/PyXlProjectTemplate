9651126683de18cfb91d5a0312ff9b2334d382e5
Dario Steiner <steiner.stdio@gmail.com>
2017-08-02 14:55:25 +0200

    Added section for executable in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
 
+from cx_Freeze import setup, Executable
 
-from distutils.core import setup
+base = None
+
+
+executables = [Executable("project_template/project_template.py", base=base)]
+
+packages = ["idna"]
+options = {
+    'build_exe': {
+
+        'packages':packages,
+    },
+
+}
 
 setup(
-    name='PyXlProjectTemplate',
-    version='0.1dev',
-    packages=['project_template', ],
-    license='Dario Steiner license',
-    long_description=open('README.md').read(),
+    name = "<any name>",
+    options = options,
+    version = "<any number>",
+    description = '<any description>',
+    executables = executables
 )
